b781d4e2ad7f57af20bc49a981e58b7a44f98989
mikelitzow <mike.litzow@noaa.gov>
2024-10-22 15:40:50 -0800

    Update

--- scripts/SDE Arctic Oscillation.R
+++ scripts/SDE Arctic Oscillation.R
@@ -5,6 +5,7 @@ library(sde)
 library(rstan)
 library(ggpubr)
 library(FactoMineR)
+library(chron)
 
 # plot settings
 theme_set(theme_bw())
@@ -28,8 +29,13 @@ pc1 <- as.matrix(hgt.anom) %*% pca$U[,1]
 # and scale!
 pc1 <- as.vector(scale(pc1))
 
+# save
+save_pc1 <- data.frame(date = chron::dates(rownames(hgt.anom)),
+                       pc1 = pc1)
+write.csv(save_pc1, "./output/pc1_height_anomaly.csv")
+
 # plot to check
-z <- temp.pca$U[,1]
+z <- pca$U[,1]
 
 z <- t(matrix(z,length(y)))  
 image(x,y,z, col=tim.colors(64))
@@ -75,7 +81,7 @@ print(ccf(sst[9:768,2], pc1.sm.9[9:768], lag.max = 10))
 # compare with ice and wind time series
 
 # load ice and wind dfa trends
-wind.ice <- read.csv("./data/wind.ice.dfa.trends.csv")
+wind.ice <- read.csv("./output/wind.ice.recruit.dfa.trends.csv")
 
 # also load Oct-Apr SE wind
 # this loads heavily on wind DFA trend1 and matches the winter season of peak AO variance
@@ -97,29 +103,25 @@ ice.out <- data.frame()
 eof1.load.out <- data.frame()
 ice.rolling.window <- data.frame()
 
-se.wind.oct.apr.out <- data.frame()
-se.wind.oct.apr.rolling.window <- data.frame()
+# se.wind.oct.apr.out <- data.frame()
+# se.wind.oct.apr.rolling.window <- data.frame()
+
+wind.trend.out <- data.frame()
+wind.trend.rolling.window <- data.frame()
 
 AO.jfm.out <- data.frame()
 AO.jfm.rolling.window <- data.frame()
 
-for(i in 1970:2013){
+for(i in 1971:2013){
   # i <- 1970
 
   temp.hgt <- hgt.anom[yr %in% c((i-20):i),] # year before to year of 20-yr window
+  # padded by one year at beginning to account for incomplete winter
   
-  temp.pca <- svd.triplet(cov(temp.hgt), col.w=weights[,1]) #weighting the columns
-  
-  temp.pc1 <- scale(as.matrix(temp.hgt) %*% temp.pca$U[,1])
-  
-  # save loadings to plot
-  eof1.load.out <- rbind(eof1.load.out,
-                         data.frame(end.year = i,
-                                    loadings = temp.pca$U[,1]))
   # separate out Nov - Mar values
   temp.m <- months(rownames(temp.hgt))
   temp.yr <- as.numeric(as.character(years(rownames(temp.hgt))))
-
+  
   # correct
   change <- temp.yr > 2030
   temp.yr[change] <- temp.yr[change] - 100
@@ -129,18 +131,27 @@ for(i in 1970:2013){
   temp.winter[temp.m %in% c("Nov", "Dec")] <- temp.winter[temp.m %in% c("Nov", "Dec")] + 1
   
   # winter only
-  temp.pc1.winter <- temp.pc1[temp.m %in% c("Nov", "Dec", "Jan", "Feb", "Mar")]
+  temp.hgt.winter <- temp.hgt[temp.m %in% c("Nov", "Dec", "Jan", "Feb", "Mar"),]
   temp.winter <- temp.winter[temp.m %in% c("Nov", "Dec", "Jan", "Feb", "Mar")]
-  temp.m <- temp.m[temp.m %in% c("Nov", "Dec", "Jan", "Feb", "Mar")]
+
+  temp.winter.pca <- svd.triplet(cov(temp.hgt.winter), col.w=weights[,1]) #weighting the columns
   
+  temp.winter.pc1 <- scale(as.matrix(temp.hgt.winter) %*% temp.winter.pca$U[,1])
+  
+  # save loadings to plot
+  eof1.load.out <- rbind(eof1.load.out,
+                         data.frame(end.year = i,
+                                    loadings = temp.winter.pca$U[,1]))
+  
+
   # winter means
-  win.pc1 <- tapply(temp.pc1.winter, temp.winter, mean)
+  win.pc1 <- tapply(temp.winter.pc1, temp.winter, mean)
   
   # and remove first and last (incomplete)
   win.pc1 <- win.pc1[2:21]
 
   # get corresponding ice time series
-  ice.temp <- as.vector(wind.ice[wind.ice$year %in% (i-19):i,2])
+  ice.temp <- as.vector(wind.ice$ice.trend[wind.ice$year %in% (i-19):i])
   
   # save ice.temp and win.pc1 to save 
   ice.rolling.window <- rbind(ice.rolling.window,
@@ -159,51 +170,72 @@ for(i in 1970:2013){
                               r = cor(ice.temp, win.pc1, use ="p"),
                               r.sq = summary(mod)$r.squared))
   
-  #####################
-  # correlation / regression for SE wind Oct-Apr
+## add wind.trend results
+  # get corresponding ice time series
+  wind.temp <- as.vector(wind.ice$wind.trend[wind.ice$year %in% (i-19):i])
   
-  # separate out Nov - Mar values
-  temp.m <- months(rownames(temp.hgt))
-  temp.yr <- as.numeric(as.character(years(rownames(temp.hgt))))
-  
-  # correct
-  change <- temp.yr > 2030
-  temp.yr[change] <- temp.yr[change] - 100
-  
-  # and advance to winter
-  temp.winter.oct.apr <- temp.yr
-  temp.winter.oct.apr[temp.m %in% c("Oct", "Nov", "Dec")] <- temp.winter.oct.apr[temp.m %in% c("Oct", "Nov", "Dec")] + 1
-  
-  # winter only
-  temp.pc1.winter.oct.apr <- temp.pc1[temp.m %in% c("Oct", "Nov", "Dec", "Jan", "Feb", "Mar", "Apr")]
-  temp.winter.oct.apr <- temp.winter.oct.apr[temp.m %in% c("Oct","Nov", "Dec", "Jan", "Feb", "Mar", "Apr")]
-  temp.m.oct.apr <- temp.m[temp.m %in% c("Oct", "Nov", "Dec", "Jan", "Feb", "Mar", "Apr")]
-  
-  # winter means
-  win.pc1.oct.apr <- tapply(temp.pc1.winter.oct.apr, temp.winter.oct.apr, mean)
-  
-  # and remove first and last (incomplete)
-  win.pc1.oct.apr <- win.pc1.oct.apr[2:21]
-  
-  # get corresponding SE wind oct apr time series
-  se.wind.temp <- as.vector(clim.dat[clim.dat$year %in% (i-19):i,3])
-  
-  # save se.wind.temp and win.pc1.oct.apr 
-  se.wind.oct.apr.rolling.window <- rbind(se.wind.oct.apr.rolling.window,
+  # save ice.temp and win.pc1 to save 
+  wind.trend.rolling.window <- rbind(wind.trend.rolling.window,
                               data.frame(end.year = i, 
                                          year = (i-19):i, 
-                                         se.wind.trend = ice.temp,
-                                         height.pc1 = win.pc1.oct.apr))
+                                         wind.trend = wind.temp,
+                                         height.pc1 = win.pc1))
   
   
   # and regress
-  mod <- lm(se.wind.temp ~ win.pc1.oct.apr)
+  mod <- lm(wind.temp ~ win.pc1)
   
-  se.wind.oct.apr.out <- rbind(se.wind.oct.apr.out,
+  wind.trend.out <- rbind(wind.trend.out,
                    data.frame(end.year = i, 
                               coef = coef(mod)[2],
-                              r = cor(se.wind.temp, win.pc1.oct.apr, use ="p"),
+                              r = cor(wind.temp, win.pc1, use ="p"),
                               r.sq = summary(mod)$r.squared))
+  
+  # #####################
+  # # correlation / regression for SE wind Oct-Apr
+  # 
+  # # separate out Nov - Mar values
+  # temp.m <- months(rownames(temp.hgt))
+  # temp.yr <- as.numeric(as.character(years(rownames(temp.hgt))))
+  # 
+  # # correct
+  # change <- temp.yr > 2030
+  # temp.yr[change] <- temp.yr[change] - 100
+  # 
+  # # and advance to winter
+  # temp.winter.oct.apr <- temp.yr
+  # temp.winter.oct.apr[temp.m %in% c("Oct", "Nov", "Dec")] <- temp.winter.oct.apr[temp.m %in% c("Oct", "Nov", "Dec")] + 1
+  # 
+  # # winter only
+  # temp.pc1.winter.oct.apr <- temp.pc1[temp.m %in% c("Oct", "Nov", "Dec", "Jan", "Feb", "Mar", "Apr")]
+  # temp.winter.oct.apr <- temp.winter.oct.apr[temp.m %in% c("Oct","Nov", "Dec", "Jan", "Feb", "Mar", "Apr")]
+  # temp.m.oct.apr <- temp.m[temp.m %in% c("Oct", "Nov", "Dec", "Jan", "Feb", "Mar", "Apr")]
+  # 
+  # # winter means
+  # win.pc1.oct.apr <- tapply(temp.pc1.winter.oct.apr, temp.winter.oct.apr, mean)
+  # 
+  # # and remove first and last (incomplete)
+  # win.pc1.oct.apr <- win.pc1.oct.apr[2:21]
+  # 
+  # # get corresponding SE wind oct apr time series
+  # se.wind.temp <- as.vector(clim.dat[clim.dat$year %in% (i-19):i,3])
+  # 
+  # # save se.wind.temp and win.pc1.oct.apr 
+  # se.wind.oct.apr.rolling.window <- rbind(se.wind.oct.apr.rolling.window,
+  #                             data.frame(end.year = i, 
+  #                                        year = (i-19):i, 
+  #                                        se.wind.trend = ice.temp,
+  #                                        height.pc1 = win.pc1.oct.apr))
+  # 
+  # 
+  # # and regress
+  # mod <- lm(se.wind.temp ~ win.pc1.oct.apr)
+  # 
+  # se.wind.oct.apr.out <- rbind(se.wind.oct.apr.out,
+  #                  data.frame(end.year = i, 
+  #                             coef = coef(mod)[2],
+  #                             r = cor(se.wind.temp, win.pc1.oct.apr, use ="p"),
+  #                             r.sq = summary(mod)$r.squared))
   # #####################
   # # AO.jfm vs. EOF1.jfm correlations
   # # separate out Jan - Mar values
@@ -302,6 +334,15 @@ ggplot(ice.out, aes(end.year, value)) +
   geom_point() +
   facet_wrap(~name, scales = "free_y", ncol = 1)
 
+wind.trend.out <- wind.trend.out %>%
+  pivot_longer(cols = -end.year)
+
+ggplot(wind.trend.out, aes(end.year, value)) +
+  geom_line() +
+  geom_point() +
+  facet_wrap(~name, scales = "free_y", ncol = 1)
+
+
 ggsave("./figs/20_yr_rolling_association_ice_trend_hgt_eof1.png", width = 4, height = 6, units = 'in')
 
 ggplot(AO.jfm.out, aes(end.year, -r)) + 
@@ -338,3 +379,64 @@ z <- t(matrix(z,length(y)))
 image(x,y,z, col=tim.colors(64))
 contour(x, y, z, add=T)  
 map('world2Hires',fill=F,add=T, lwd=2)
+
+### add AO - wind/ice correlation----------
+
+# load ice and wind dfa trends
+wind.ice <- read.csv("./output/wind.ice.recruit.dfa.trends.csv")
+
+# also load canonical AO 
+clim.dat <- read.csv("./data/Bering climate data.csv")
+clim.dat <- clim.dat %>%
+  dplyr::select(year, AO.jfm)
+
+dat <- wind.ice %>%
+  dplyr::select(-recruit.trend) %>%
+  left_join(., clim.dat)
+
+# rolling window
+plot_out <- data.frame()
+
+for(i in 1970:2013){
+  # i <- 1970
+  temp <- dat %>%
+    filter(year %in% i:(i-19))
+  
+  wind.mod <- lm(wind.trend ~ AO.jfm, data = temp)
+  
+  ao.wind <- data.frame(window.end = i,
+                        r = cor(temp$AO.jfm, temp$wind.trend),
+                        coef = coef(wind.mod)[2],
+                        variable = "Wind trend")
+  
+  ice.mod <- lm(ice.trend ~ AO.jfm, data = temp)
+  
+  ao.ice <- data.frame(window.end = i,
+                        r = cor(temp$AO.jfm, temp$ice.trend),
+                        coef = coef(ice.mod)[2],
+                        variable = "Ice trend")
+  
+  plot_out <- rbind(plot_out,
+                    ao.wind,
+                    ao.ice)
+}
+
+
+ggplot(plot_out, aes(window.end, coef)) +
+  geom_point() +
+  geom_line() +
+  facet_wrap(~variable, scales = "free_y", ncol = 1)
+
+
+plot_out <- plot_out %>%
+  pivot_longer(cols = -window.end) 
+
+ggplot(plot_out, aes(window.end, value, color = name)) +
+  geom_hline(yintercept = 0, color = "dark grey") +
+  geom_line() +
+  geom_point() + 
+  labs(x = "Window end",
+       y = "Correlation coefficient") +
+  scale_color_manual(values = c("black", cb[7])) +
+  scale_y_continuous(breaks = seq(-0.75, 0.75, 0.25)) +
+  theme(legend.title = element_blank())
